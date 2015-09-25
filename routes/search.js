719679c9fcf0dbe8be04038f2f7923f61b9714f9
Albin Theander <Albin.Theander@jayway.com>
2015-09-25 10:33:24 +0200

    Added search

--- routes/search.js
+++ routes/search.js
@@ -8,25 +8,28 @@ var users = [];
 
 var cardPath = path.join(__dirname, '../data/jayway-people.vcf');
 var vCardData = fs.readFileSync(cardPath, 'utf-8');
+
+// The vcard library can only parse one vcard at a time, so we
+// need to split the data into individual vcards and convert
+// them one by one
 vCardData.split(/\n(?=BEGIN:VCARD)/g).map(function(vCardPerson, i) {
 	vCard.readData(vCardPerson, function(err, json) {
+		// Error handling, you say? What can possibly go wrong with this?
 		users.push(json);
+		console.log(json);
 	});
 });
-// console.log(cardPath);
-// vCard.readFile(path.join(__dirname, '../data/jayway-people.vcf'), function(err, json) {
-// 	console.log(err, json);
-// });
 
-/* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('search');
+  res.render('search', {title: 'Search'});
 });
 
 router.post('/result', function(req, res) {
-  	res.render('users', users.filter(function(user) {
-  		return user.FN.indexOf(req.body.name) >= 0;
-  	}));
+	var searchRegExp = new RegExp(req.body.name, 'ig');
+	var matchingUsers = users.filter(function(user) {
+  		return searchRegExp.test(user.FN);
+  	});
+  	res.render('searchresult', { people: matchingUsers });
 });
 
 module.exports = router;
