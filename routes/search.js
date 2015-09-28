2fb1706839d5926a5a8e9cc8c7ed61131bec1c34
Albin Theander <Albin.Theander@jayway.com>
2015-09-28 13:08:06 +0200

    Refactored search a bit

--- routes/search.js
+++ routes/search.js
@@ -1,34 +1,17 @@
 var express = require('express');
-var path = require('path');
-var vCard = new (require('vcard'))();
 var router = express.Router();
-var fs            = require('fs');
+var Users = require('../lib/users/users.js');
 
-var users = [];
-
-var cardPath = path.join(__dirname, '../data/jayway-people.vcf');
-var vCardData = fs.readFileSync(cardPath, 'utf-8');
-
-// The vcard library can only parse one vcard at a time, so we
-// need to split the data into individual vcards and convert
-// them one by one
-vCardData.split(/\n(?=BEGIN:VCARD)/g).map(function(vCardPerson, i) {
-	vCard.readData(vCardPerson, function(err, json) {
-		// Error handling, you say? What can possibly go wrong with this?
-		users.push(json);
-	});
-});
 
 router.get('/', function(req, res) {
   res.render('search', {title: 'Search'});
 });
 
 router.post('/result', function(req, res) {
-	var searchRegExp = new RegExp(req.body.name, 'ig');
-	var matchingUsers = users.filter(function(user) {
-  		return searchRegExp.test(user.FN);
-  	});
-  	res.render('searchresult', { people: matchingUsers });
+  var query = req.body.name;
+  res.render('searchresult', {
+    people: Users.search(query)
+  });
 });
 
 module.exports = router;
